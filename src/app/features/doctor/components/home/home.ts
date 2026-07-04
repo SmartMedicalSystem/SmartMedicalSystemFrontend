e96a104eea32c2dde808900d03005478eb1ce76d
mohaed tarek <fcb6480@gmail.com>
2026-07-05 00:17:54 +0300

    Admin bascis

---
+++ src/app/features/doctor/components/home/home.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+}
