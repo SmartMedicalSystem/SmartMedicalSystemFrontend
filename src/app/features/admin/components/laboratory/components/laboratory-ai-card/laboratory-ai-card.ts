3a3f55b368c9fe3807d131fd4d30f42d8b783986
mohaed tarek <fcb6480@gmail.com>
2026-07-06 01:19:43 +0300

    laboratory for Admin Component

---
+++ src/app/features/admin/components/laboratory/components/laboratory-ai-card/laboratory-ai-card.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory-ai-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './laboratory-ai-card.html',
+  styleUrl: './laboratory-ai-card.css'
+})
+export class LaboratoryAiCard {
+
+}
---
+++ src/app/features/admin/components/laboratory/components/laboratory-header/laboratory-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory-header',
+  standalone: true,
+  templateUrl: './laboratory-header.html',
+  styleUrl: './laboratory-header.css'
+})
+export class LaboratoryHeader {
+
+}
