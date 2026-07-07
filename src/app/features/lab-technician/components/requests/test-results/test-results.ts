0be88222f16b890635f93ac1188cffe17763def1
Salwa <salwasayed522@gmail.com>
2026-07-07 22:28:14 +0300

    Add laboratory requests/ results/profile

--- src/app/features/lab-technician/components/requests/test-results/test-results.ts
+++ src/app/features/lab-technician/components/requests/test-results/test-results.ts
@@ -1,9 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-test-result',
-  imports: [],
+  selector: 'app-test-results',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './test-results.html',
-  styleUrl: './test-results.css',
+  styleUrls: ['./test-results.css']
 })
-export class TestResults { }
+export class TestResults {
+
+  analytes = [
+
+    {
+      name: 'Hemoglobin (Hb)',
+      value: '12.1',
+      unit: 'g/dL',
+      reference: '13.5 - 17.5',
+      status: 'Low'
+    },
+
+    {
+      name: 'White Blood Cells (WBC)',
+      value: '7.2',
+      unit: 'x10³/µL',
+      reference: '4.0 - 11.0',
+      status: 'Normal'
+    },
+
+    {
+      name: 'Platelets',
+      value: '122',
+      unit: 'x10³/µL',
+      reference: '150 - 450',
+      status: 'Critical Low'
+    },
+
+    {
+      name: 'Glucose (Fasting)',
+      value: '105',
+      unit: 'mg/dL',
+      reference: '70 - 110',
+      status: 'Normal'
+    }
+
+  ];
+
+}
